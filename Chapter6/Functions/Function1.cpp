84cd2aa6967a9e5394c5e904909f21427309fed2
PepeLePuu <redtition@hotmail.com>
2014-02-11 14:43:11 -0800

    Create Function1.cpp

---
+++ Chapter6/Functions/Function1.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+void print();
+int main()
+{
+  cout << "Hello World\n";
+  print();
+  system("pause");
+  return 0;
+}
+void print()
+{
+  cout << "\nToday is the first day of school!\n";
+}
