3d9545fa901cfea6aa27ca41512ba11c0d4232be
PepeLePuu <redtition@hotmail.com>
2014-02-13 13:12:03 -0800

    Update countdown.c

--- Chapter6/Bryant_Practice/Week_One/countdown.c
+++ Chapter6/Bryant_Practice/Week_One/countdown.c
@@ -4,14 +4,13 @@
 
 void print_count(int n)
 {
-printf("T minus %d and counting\n", n);
+    printf("T minus %d and counting\n", n);
 }
 
 main()
 {
-int i;
-
-for(int = 10; i > 0; --i)
-print_count(i);
-return 0;
+  int i;
+  for(int = 10; i > 0; --i)
+  print_count(i);
+  return 0;
 }
