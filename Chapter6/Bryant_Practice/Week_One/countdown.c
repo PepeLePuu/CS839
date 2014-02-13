42bcdb23181444aeac15eb5996bbd8ded609be7e
PepeLePuu <redtition@hotmail.com>
2014-02-13 13:10:51 -0800

    Create countdown.c

---
+++ Chapter6/Bryant_Practice/Week_One/countdown.c
@@ -0,0 +1,17 @@
+/*Prints a countdown*/
+
+#include <stdio.h>
+
+void print_count(int n)
+{
+printf("T minus %d and counting\n", n);
+}
+
+main()
+{
+int i;
+
+for(int = 10; i > 0; --i)
+print_count(i);
+return 0;
+}
