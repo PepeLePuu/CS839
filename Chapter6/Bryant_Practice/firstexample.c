9850c995ab7c233b6010277501468cde682c0208
PepeLePuu <redtition@hotmail.com>
2014-02-13 12:34:27 -0800

    Create firstexample.c

---
+++ Chapter6/Bryant_Practice/firstexample.c
@@ -0,0 +1,5 @@
+#include <stdio.h>
+float average(float a, float b)/*a type like "float" is the type of data that the functions return each time it's called.*/
+{
+return (a + b) / 2;/*the body of a function must have a return to pass the argument(s) within in(eg. a, b)*/
+}
