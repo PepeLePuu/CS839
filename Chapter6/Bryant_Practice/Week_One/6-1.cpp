431c1c0dfea35bda6fe478ead86f3641d9d99b62
PepeLePuu <redtition@hotmail.com>
2014-02-14 14:56:44 -0800

    Update 6-1.cpp

--- Chapter6/Bryant_Practice/Week_One/6-1.cpp
+++ Chapter6/Bryant_Practice/Week_One/6-1.cpp
@@ -8,18 +8,17 @@ using namespace std;
 ****************************************/
 void displayMessage()
 {
-cout << "Hello from the function displayMessage.\n";
+  cout << "Hello from the function displayMessage.\n";
 }
 /***************************************
 *
-main
+              main
 *
 ***************************************/
 int main()
 {
-cout << "Hello from main.\n";
-displayMessage();
-// Call displayMessage
-cout << "Back in function main again.\n";
-return 0;
+              cout << "Hello from main.\n";
+              displayMessage();// Call displayMessage
+              cout << "Back in function main again.\n";
+              return 0;
 }
