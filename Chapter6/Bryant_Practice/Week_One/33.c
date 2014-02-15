b5c9e025c9fd1d1894c156f4145be00e56c242bb
PepeLePuu <redtition@hotmail.com>
2014-02-14 22:51:53 -0800

    Create 33.c

---
+++ Chapter6/Bryant_Practice/Week_One/33.c
@@ -0,0 +1,12 @@
+/*The following statement calls a function named 'half', which return a value that is half that of the argument passed to itt.
+Assume that 'result' and 'number' have both been defined to be double variables. Write the half function
+
+
+result = half(number);
+*/
+include <stdio.h>
+main()
+{
+
+  return 0;
+}
