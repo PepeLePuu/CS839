db6796489b7a8955218ac227b5b0992fa5b7ece1
PepeLePuu <redtition@hotmail.com>
2014-02-18 14:43:10 -0800

    Update thirdexample.cpp

--- Chapter6/Functions/Week_Two/thirdexample.cpp
+++ Chapter6/Functions/Week_Two/thirdexample.cpp
@@ -18,5 +18,5 @@ void modify(char& gr)//char reference (&)
   char letter = toupper(gr);
   
   if(!(letter < 'A' || letter > 'F' || letter != 'E'));//if not between A and F excluding E
-  gr = 'F';
+  	gr = 'F';
 }
