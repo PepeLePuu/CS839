0fee4fc3447e4d0e752fb9272102e229441b508c
PepeLePuu <redtition@hotmail.com>
2014-02-18 14:51:17 -0800

    Update thirdexample.cpp

--- Chapter6/Functions/Week_Two/thirdexample.cpp
+++ Chapter6/Functions/Week_Two/thirdexample.cpp
@@ -2,21 +2,19 @@
 using namespace std;
 void modify(char&);//pass by reference
 int main()
-{
+{	
 	char grade;
+
 	cout << "Enter a letter grade: \n";
 	cin >> grade;
-	
-	modify(grade);
-	cout << "Grade = \n";
+	modify(grade);	
+	cout << "Grade = " << grade << endl;
 
 	system("pause");
 	return 0;
-}
-void modify(char& gr)//char reference (&)
-{
-  char letter = toupper(gr);
-  
-  if(!(letter < 'A' || letter > 'F' || letter != 'E'));/*if not between A and F excluding E*/
-  gr = 'F';
+}void modify(char& gr)//char reference (&)
+{  
+	char letter = toupper(gr);  
+	if(!(letter < 'A' || letter > 'F' || letter != 'E'));/*if not between A and F excluding E*/  
+	gr = 'F';
 }
