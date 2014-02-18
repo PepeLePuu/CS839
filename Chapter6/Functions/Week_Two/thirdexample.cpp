97b99ed4f2533bf660e08052f491bef6deb10df2
PepeLePuu <redtition@hotmail.com>
2014-02-18 14:42:37 -0800

    Create thirdexample.cpp

---
+++ Chapter6/Functions/Week_Two/thirdexample.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+using namespace std;
+void modify(char);
+int main()
+{
+	char letter;
+	cout << "Enter a letter grade: \n";
+	cin >> letter;
+	
+	modify(grade);
+	cout << "Grade = \n";
+
+	system("pause");
+	return 0;
+}
+void modify(char& gr)//char reference (&)
+{
+  char letter = toupper(gr);
+  
+  if(!(letter < 'A' || letter > 'F' || letter != 'E'));//if not between A and F excluding E
+  gr = 'F';
+}
