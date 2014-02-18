a3e18ff1a2ad0443ec419904d0492d41ffa9ca44
PepeLePuu <redtition@hotmail.com>
2014-02-18 09:35:57 -0800

    Update firstexcercise.c

--- Chapter6/Bryant_Practice/Ch6Exercises/firstexcercise.c
+++ Chapter6/Bryant_Practice/Ch6Exercises/firstexcercise.c
@@ -1,6 +1,16 @@
 #include <stdio.h>
+int half(double number)
+{
+  return number / 2;
+}
 main()
 {
-
-return 0;
+  double number, result;
+  
+  printf("Enter a  number:\n");
+  scanf("%d",&number)
+  
+  result = half(number);
+  printf("The half of this number is %d", result);
+  return 0;
 }
