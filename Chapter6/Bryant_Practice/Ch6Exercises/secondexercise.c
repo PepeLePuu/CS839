fc82e97397b698a85069a78be624be85bb2548fd
PepeLePuu <redtition@hotmail.com>
2014-02-18 11:56:36 -0800

    Create secondexercise.c

---
+++ Chapter6/Bryant_Practice/Ch6Exercises/secondexercise.c
@@ -0,0 +1,17 @@
+/*34. A program contains the following function.
+int cube(int num)
+{
+return num * num * num;
+}
+Write a statement that passes the value 4 to this function and assigns its return value
+to the variable result .*/
+#include <stdio.h>
+int cube(int num)
+{
+  return num * num * num;
+}
+main()
+{
+
+  return 0;
+}
