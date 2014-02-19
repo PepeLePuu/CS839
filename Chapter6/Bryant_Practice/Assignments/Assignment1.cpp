f7406d55cbc2325f43d0b09c76598a2eb52c81cd
PepeLePuu <redtition@hotmail.com>
2014-02-18 16:44:57 -0800

    Create Assignment1.cpp

---
+++ Chapter6/Bryant_Practice/Assignments/Assignment1.cpp
@@ -0,0 +1,26 @@
+/*Bryant Zepeda
+CSIT 839
+Assignment #1
+*/
+#include <iostream>
+bool reduce()
+{
+
+void calc(int&, int&);
+using namespace std;
+int main()
+{
+	int psr_num1, psr_den1, psr_num2, psr_den2;
+	char slash = '/';
+	char operation;
+
+	cout << "Enter a fraction expression: ";
+	cin >> psr_num1 >> slash >> psr_den1
+		>> psr_num2 >> slash >> psr_den2;
+
+
+	system("pause");
+	return 0;
+}
+void calc(int& num, int& den)
+{
