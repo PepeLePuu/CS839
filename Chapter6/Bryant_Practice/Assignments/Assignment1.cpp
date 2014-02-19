8ad757167e8971c9a54a799c1797321bbc1a994a
PepeLePuu <redtition@hotmail.com>
2014-02-19 09:31:06 -0800

    Update Assignment1.cpp

--- Chapter6/Bryant_Practice/Assignments/Assignment1.cpp
+++ Chapter6/Bryant_Practice/Assignments/Assignment1.cpp
@@ -1,4 +1,4 @@
-]/*Bryant Zepeda
+/*Bryant Zepeda
 CSIT 839
 Assignment #1
 */
@@ -15,23 +15,27 @@ int psr_num1, psr_den1, psr_num2, psr_den2;
 char slash = '/';
 
 char operation;
-
 cout << "Enter a fraction expression: ";
-cin >> psr_num1 >> slash >> psr_den1
->> psr_num2 >> slash >> psr_den2;
+ cin >> psr_num1 >> slash >> psr_den1 >>
+
+operation >> psr_num2 >> slash >> psr_den2;
 
 calc(psr_num1, psr_den1, psr_num2, psr_den2);
+
+ cout << 
 
 //system("pause");
 return 0;
 }
 void calc(int& num1, int& den1, int& num2, int& den2)
 {
-char operation;
+  char operation; int t_num, t_den;
 switch(operation)
 {
- case 1: 
+ case 1:
 if(operation == '+')
+  t_num = num1 + num2;
+ t_den = den1 + den2;
  break;
  case 2:
 if(operation == '*')
