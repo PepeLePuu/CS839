51dfdf5811f406d0e8d523d459c6b26aa2adb793
PepeLePuu <redtition@hotmail.com>
2014-02-22 10:59:21 -0800

    Update secondexample.cpp

--- Chapter6/Functions/Week_Two/secondexample.cpp
+++ Chapter6/Functions/Week_Two/secondexample.cpp
@@ -1,65 +1,21 @@
-/*Bryant Zepeda
-CSIT 839
-Assignment #1
-*/
 #include <iostream>
 using namespace std;
-bool reduce()
+bool grade(char a)//bool or true or false
 {
-
-};
-void calc(int&, int&, char&, int&, int&, int&, int&);
+char letter = toupper(a);
+return letter = ((letter >= 'A' && letter <= 'D') || letter == 'F');//return 0 or 1
+}
 int main()
 {
-int psr_num1, psr_den1, psr_num2, psr_den2;
-char slash = '/';
+char letter;
+cout << "Enter a letter grade: \n";
+cin >> letter;
 
-char operation;
-cout << "Enter a fraction expression: ";
- cin >> psr_num1 >> slash >> psr_den1 >>
+if(grade(letter))
+cout << "Valid grade!\n";
+else
+cout << "\nInvalid grade\n";
 
-operation >> psr_num2 >> slash >> psr_den2;
-
- int store_num, store_den;
- calc(psr_num1, psr_den1, operation, psr_num2, psr_den2, store_num, store_den);
-
- cout << psr_num1 << slash << psr_den1 << operation << psr_num2
-      << slash << psr_den2 << " = " << store_num << slash << store_den 
-      << endl;
-
-
-//system("pause");
+system("pause");
 return 0;
-}
-void calc(int& num1, int& den1, char& op_calc, int& num2, int& den2, int& calln, int& calld)
-{
- int t_num, t_den;
-if(op_calc == '+')
-  {
- t_num = num1 + num2;
- t_den = den1 + den2;
- calln = t_num;
- calld = t_den;
-  }
-if(op_calc == '-')
-  {
-    t_num = num1 - num2;
-    t_den = den1 - den2;
-    calln = t_num;
-    calld = t_den;
-  }
-if(op_calc == '*')
-  {
-    t_num = num1 * num2;
-    t_den = den1 * den2;
-    calln = t_num;
-    calld = t_den;
-  }
-if(op_calc == '/')
-  {
-    t_num = num1 * den2;
-    t_den = num2 * den1;
-    calln = t_num;
-    calld = t_den;
-  }
 }
