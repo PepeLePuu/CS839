90cea5ce8860beeee6b74ac7e07dd67212d84e8c
PepeLePuu <redtition@hotmail.com>
2014-02-25 14:07:27 -0800

    Create fourthexample.cpp

---
+++ Chapter6/Functions/Week_Three/fourthexample.cpp
@@ -0,0 +1,24 @@
+/*#include <iostream>
+#include <string>
+using namespace std;
+int main()
+{
+	char city[15], name[15];
+
+	cout << "Enter your name: \n";
+	cin.getline(name, 15, '\n');
+	cout << "Enter city: \n";
+	cin.getline(city, 15, '\n');
+	cout << name << endl << city;
+
+	strcpy(city, name);
+	cout << endl << name << endl << city;
+
+	strcat(city, name);
+	cout << endl << name << endl << city;
+
+	//strcpy(city, name);Copy
+	//strcat(city, name); Append/Conconnotation 
+	system("pause");
+	return 0;
+}*/
