2e2aac284440a1c9183242b11d5a013f7b2be9a6
PepeLePuu <redtition@hotmail.com>
2014-02-25 14:51:28 -0800

    Create EightExample.cpp

---
+++ Chapter6/Functions/Week_Three/EightExample.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+#include <string>
+using namespace std;
+int main()
+{
+	char ch, ch2;
+
+	cout << "Enter a character: ";
+	cin >> ch;//cin ignores whitespace and ignore the new lines the user enters
+
+	cout << "\nEnter another character: ";
+	cin >> ch2;
+
+	system("pause");
+	return 0;
+}
