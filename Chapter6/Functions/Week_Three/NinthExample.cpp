35a687217cbe4f51248ee0a4380a9bc418c4212e
PepeLePuu <redtition@hotmail.com>
2014-02-25 14:52:40 -0800

    Create NinthExample.cpp

---
+++ Chapter6/Functions/Week_Three/NinthExample.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+#include <string>
+using namespace std;
+int main()
+{
+	char ch, ch2;
+
+	cout << "Enter a character: ";
+	cin.get(ch);
+
+	cout << "\nEnter another character: ";
+	cin.get(ch2);
+
+	system("pause");
+	return 0;
+}
