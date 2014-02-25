de272ff01b3cc09695bf467593969b2b3226c8ec
PepeLePuu <redtition@hotmail.com>
2014-02-25 15:00:48 -0800

    Update NinthExample.cpp

--- Chapter6/Functions/Week_Three/NinthExample.cpp
+++ Chapter6/Functions/Week_Three/NinthExample.cpp
@@ -7,9 +7,12 @@ int main()
 
 	cout << "Enter a character: ";
 	cin.get(ch);
+	cin.ignore();
 
 	cout << "\nEnter another character: ";
-	cin.get(ch2);
+	cin.get(ch2);//Before a cin.get() use have to use cin.ignore after it.
+
+	cout << ch << endl << ch2 << endl;
 
 	system("pause");
 	return 0;
