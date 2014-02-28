48c739b90d7439774293be1169cae7d1654417c7
PepeLePuu <redtition@hotmail.com>
2014-02-28 13:40:41 -0800

    Update assignment1.cpp

--- Chapter6/Bryant_Practice/Assignments/assignment1.cpp
+++ Chapter6/Bryant_Practice/Assignments/assignment1.cpp
@@ -12,10 +12,7 @@ For example, if the fraction is 12/9, try dividing 12 by 9, if it were divisible
 you'd be done, but since it's not you try 12 over 8, 7, 6, ..., 3;
 when you get to 3, they're both divisible so you divide both by 3 getting 4/3.*/
 
-
 void calc(int&,char&,int&, char&, int&, char&, int&, int&, int&);
-
-//----------------------------------------------------------------<INT CALC> 
 int main()
 {
   int psr_num1, psr_den1, psr_num2, psr_den2;
@@ -62,8 +59,6 @@ operation >> psr_num2 >> slash >> psr_den2;
 //system("pause");
        return 0;
 }
-
-//----------------------------------------------------------------<VOID CALC> 
 void calc(int& num1, char& slash1, int& den1, char& op_calc, int& num2, char& slash2, int& den2, int& calln, int& calld)
 {
 if(op_calc == '+')
@@ -89,46 +84,18 @@ if(op_calc == '/')
   }
 }
 
-
-
-//----------------------------------------------------------------<BOOL REDUCE> 
 bool reduce(int& r_num, int& r_den, bool& r_tf)
 {
-for(int i = r_num < r_den ? r_num : r_den; i <= 1; --i)
+for(int i = r_num < r_den ? r_num : r_den; i >= 1; --i)
   {
-    if(i == 1)
-      {
-	r_tf = false;
-      }
-    else if(r_num % i == 0 && r_den % i == 0)
-      {
-	r_num/=i;
-	r_den/=i;
-	r_tf = true;   
-      }
-  }
- return r_tf;
-}
-
-/*!PROBLEM!: Floating point exeptions happen when multiplying 1/2 * 1/4 when the boolean paramater for
-reduce(int, int, bool) is set to decide whether it is true or false[LINE 89-105]. When boolean conditions
-are removed, the program runs flawlessly. Most likely a flow&bracket error after the for statement
-when declaring bool tf*/
-
-/*bool reduce(int& r_num, int& r_den, bool& r_tf)
-{
-for(int i = r_num < r_den ? r_num : r_den; i <= 1; --i)
 if(r_num % i == 0 && r_den % i == 0)
   if(i == 1)
     {
       r_tf = false;
     }
-    else
-      {
 	r_num/=i;
 	r_den/=i;
-	r_tf = true;   
-      }
+	r_tf = true;
+  }   
  return r_tf;
 }
-*/
