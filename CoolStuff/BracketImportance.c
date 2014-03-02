2fed74e23a18e4d4f1a5a3d1e7722569eb163c01
PepeLePuu <redtition@hotmail.com>
2014-03-01 22:12:11 -0800

    Create BracketImportance.c

---
+++ CoolStuff/BracketImportance.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+main()
+{
+if(1==2)
+printf("1==2, This is without brackets\n");
+
+if(1==2)
+{
+printf(1==2, this is WITH brackets\n");
+}
+else if(1!=2)
+{
+printf("See what happened?\n");
+}
+return 0;
+}
