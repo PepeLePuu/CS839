2e07a7655844de8d596c2b78db0b79433a20fea9
PepeLePuu <redtition@hotmail.com>
2014-03-03 12:44:56 -0800

    Quickly done.
    More comments are needed for basic inputs

--- Chapter6/Bryant_Practice/Assignments/BryantZ2Assignment.cpp
+++ Chapter6/Bryant_Practice/Assignments/BryantZ2Assignment.cpp
@@ -1,8 +1,30 @@
 #include <iostream>
 using namespace std;
+bool search(char&);/*I want to use a search funtion that will use each letter as an array and search through them using a for statement*/
+void select();/*selection menu*/
 int main()
 {
+  int option;
+  cout << "Please enter a string: ";
 
-/*system("pause");*/
-return 0;
+  cout << endl << endl << "Make a selection\n";
+  select();
+  cin >> option;
+  cin.ignore();/*I don't want anything else streaming to the next input*/
+
+  /*system("pause");*/
+  return 0;
 }
+void select()
+{
+  cout << "1. Search for a character in the string\n";
+  cout << "2. Change a character within the string\n";
+  cout << "3. Display the first n characters of the string\n";
+  cout << "4. Display the last n characters of the string\n";
+  cout << "5. Display all characters that lie betwen two given indices\n";
+  cout << "6. Null the string\n";
+  cout << "7. Exit\n";
+
+}
+
+
