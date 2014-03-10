56d8118c44677e9471dcb0ef5d9f49a4d08215d6
PepeLePuu <redtition@hotmail.com>
2014-03-09 19:46:48 -0700

    Create PassArrLoop.cpp

---
+++ Chapter6/Assignments/Assignment2/PassArrLoop.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+using namespace std;
+void CreateDefinedArray(char[]);
+int main()
+{
+  char NonDefinedArray[10];
+  char NotYetStoredLetter;
+
+  CreateDefinedArray(NonDefinedArray);
+
+  for(int Increment = 0; Increment <= 10; ++Increment)
+    {
+      cout << NonDefinedArray[Increment] << ' ';
+    }
+  cout << endl;
+
+  return 0;
+}
+void CreateDefinedArray(char DefineArray[10])
+{
+  char StartingLetter = 'a';
+
+  for(int Increment = 0; Increment <= 10; ++Increment)
+    {
+      DefineArray[Increment] = StartingLetter;
+      StartingLetter++;
+    }
+}
