66c688d3875e23bbba93d7038120f81b4b6924c6
PepeLePuu <redtition@hotmail.com>
2014-03-10 02:12:42 -0700

    Broken code for printing the first characters

---
+++ Chapter6/Assignments/Assignment2/PrintFirstIndexv1.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+using namespace std;
+void CreateDefinedArray(char[], int&);
+int main()
+{
+  char NonDefinedArray[50]; int StartingIndex;
+
+  cout << "Enter a string: ";
+  cin.getline(NonDefinedArray, 50, '\n');
+
+  cout << "How many characters from the start do you want to display?";
+  cin >> StartingIndex;
+
+  CreateDefinedArray(NonDefinedArray, StartingIndex);
+
+  cout << NonDefinedArray << endl;
+
+  return 0;
+}
+void CreateDefinedArray(char DefineArray[50], int& StartingIndex)
+{
+  for(int IncrementalDelete = StartingIndex; DefineArray[StartingIndex] <= DefineArray['\0']; ++IncrementalDelete)
+    {
+      DefineArray[StartingIndex] = DefineArray['\0'];
+    }
+}
